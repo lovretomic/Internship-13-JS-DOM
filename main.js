403e50e0b78ce9de55034bd2bc12eb2af8551e1c
lovretomic <tomiclovre05@gmail.com>
2023-02-19 12:34:18 +0100

    Updated dropdown selection

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const dropdownInputs = document.querySelectorAll(
-  ".main__form-box-input.dropdown"
-);
-const dropdowns = document.querySelectorAll(".main__form-box-input ~ div");
+const dropdownInputs = document.querySelectorAll('.main__form-box-input.dropdown');
+const dropdowns = document.querySelectorAll('.main__form-box-input ~ div');
+const dropdownOptions = document.querySelectorAll('.dropdown-select-option');
 
 console.log(dropdownInputs);
 console.log(dropdowns);
+console.log(dropdownOptions);
 
 const dropdownStatus = {
   level: false,
@@ -20,6 +20,27 @@ function toggleDropdown(index) {
     dropdowns[index].style.display = 'none';
     dropdownStatus[keys[index]] = false;
   }
+}
+
+function insertDropdownOption(index) {
+    const optionValue = dropdownOptions[index].innerHTML;
+    console.log(optionValue);
+    if (index < 3) dropdownInputs[0].value = optionValue;
+    else dropdownInputs[1].value = optionValue;
+}
+
+for (let i = 0; i < dropdownOptions.length; i++) {
+    dropdownOptions[i].addEventListener('mouseover', () => {
+        dropdownOptions[i].style.color = '#1360a0';
+    });
+
+    dropdownOptions[i].addEventListener('mouseout', () => {
+        dropdownOptions[i].style.color = 'black';
+    });
+    
+    dropdownOptions[i].addEventListener('mousedown', () => {
+        insertDropdownOption(i);
+    });
 }
 
 for (let i = 0; i < dropdownInputs.length; i++) {
